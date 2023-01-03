3788e3f431ffc9371e00d7a2a65af92172861d2c
imrana abdulkadir <imranaabdulkadir6@gmail.com>
2023-01-03 12:35:01 -0800

    hood

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,1 @@
-#!/usr/bin/python3
-for character in range(97, 123):
-    print("{:c}".format(character), end='')
+#!/usr/bin/python3for character in range(97, 123):    print("{:c}".format(character), end='')
